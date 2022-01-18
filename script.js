1eeacfab60ca9c3757f0fb84242b23bac87fc0d4
Guilherme Oliani <guilhermenoliani@gmail.com>
2022-01-18 13:56:12 -0300

    Initial Data

---
+++ script.js
@@ -0,0 +1,12 @@
+// Initial Data
+let square = {
+    a1 = '', a2 = '', a3 = '',
+    b1 = '', b2 = '', b3 = '',
+    c1 = '', c2 = '', c3 = ''
+};
+let playerTurn = '';
+let warning = '';
+let playing = false;
+// Events
+
+// Functions
