f5267afdab8896d59b9554c96179e42d22f30da6
Guilherme Oliani <guilhermenoliani@gmail.com>
2022-01-18 15:34:51 -0300

    TS

--- public/script.js
+++ public/script.js
@@ -3,39 +3,50 @@ var square = {
     b1: '', b2: '', b3: '',
     c1: '', c2: '', c3: ''
 };
-var playerTurn = '';
+var playerTurn = 'x';
 var warning = '';
 var playing = false;
+reset();
 // Events
 document.querySelector('.reset').addEventListener('click', reset); // Adicionando uma ação no botão reset
 document.querySelectorAll('.item').forEach(function (item) {
-    item.addEventListener('click', itemClick);
+    item.addEventListener('click', function (e) {
+        var loc = e.target.getAttribute('data-item');
+        if (playing && square[loc] === '') {
+            square[loc] = playerTurn;
+            renderSquare();
+            togglePlayer();
+        }
+    });
 }); // Adicionando evento de click em todos os itens
 // Functions
-function itemClick(event) {
-    var item = event.target.getAtrribute('data-item');
-    if (square[item] === '') {
-        square[item] = playerTurn;
-    }
-}
 function reset() {
     warning = '';
     var random = Math.floor(Math.random() * 2); //Random player
-    playerTurn = (random === 0) ? 'x' : 'o'; // 'x'  or  'o'
+    playerTurn = random === 0 ? 'x' : 'o'; // 'x'  or  'o'
     for (var i in square) { // Cleaning the square
         square[i] = '';
     }
-    playing = true;
     renderSquare();
     renderInfo();
+    playing = true;
 }
 function renderSquare() {
     for (var i in square) {
         var item = document.querySelector("div[data-item=".concat(i, "]"));
-        item.innerHTML = square[i];
+        if (square[i] !== '') {
+            item.innerHTML = square[i];
+        }
+        else {
+            item.innerHTML = '';
+        }
     }
 }
 function renderInfo() {
     document.querySelector('.vez').innerHTML = playerTurn;
     document.querySelector('.resultado').innerHTML = warning;
+}
+function togglePlayer() {
+    playerTurn = (playerTurn === 'x') ? 'o' : 'x';
+    renderInfo();
 }
--- src/script.ts
+++ src/script.ts
@@ -3,45 +3,66 @@ let square = {
     b1: '', b2: '', b3: '',
     c1: '', c2: '', c3: ''
 };
-let playerTurn = '';
+let playerTurn = 'x';
 let warning = '';
 let playing = false;
+
+reset();
+
 // Events
 document.querySelector('.reset').addEventListener('click', reset); // Adicionando uma ação no botão reset
-document.querySelectorAll('.item').forEach(item =>{ //Percorre todos os itens
-    item.addEventListener('click', itemClick);
+
+document.querySelectorAll('.item').forEach((item) =>{ //Percorre todos os itens
+    item.addEventListener('click', (e) => {
+        let loc = e.target.getAttribute('data-item');
+        if(playing && square[loc] === ''){
+            square[loc] = playerTurn;
+            renderSquare();
+            togglePlayer();
+        }
+    });
 }); // Adicionando evento de click em todos os itens
 
 // Functions
-function itemClick(event){
-    let item = event.target.getAtrribute('data-item');
-    if(square[item] === ''){
-        square[item] = playerTurn;
-    }
-}
+
 function reset(){
     warning = '';
 
     let random = Math.floor(Math.random() * 2); //Random player
-    playerTurn = (random === 0) ? 'x' : 'o'; // 'x'  or  'o'
+    playerTurn = random === 0 ? 'x' : 'o'; // 'x'  or  'o'
    
     for(let i in square){ // Cleaning the square
         square[i] = '';
     }
 
-    playing = true;
 
     renderSquare();
     renderInfo();
+
+    playing = true;
+
+    
 }
 
 function renderSquare(){
+
     for(let i in square){
         let item = document.querySelector(`div[data-item=${i}]`)
-        item.innerHTML = square[i]
-    }
+        if(square[i] !== ''){
+            item.innerHTML = square[i];
+        } else {
+            item.innerHTML= '';
+        }
+        
+    } 
 }
 function renderInfo(){
     document.querySelector('.vez').innerHTML = playerTurn;
     document.querySelector('.resultado').innerHTML = warning;
+}
+
+function togglePlayer(){
+    
+    playerTurn = (playerTurn === 'x') ? 'o' : 'x';
+    renderInfo();
 }
